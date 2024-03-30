c3e8a5961e47da53d112522e27586f7734b265d0
Alexis Degrugillier <github@ainw.org>
2024-03-30 13:56:21 -0400

    Replace local library by CDN library
    This was necessary since the library is loading other resources. As the loading
    process in the extension is not designed for that, it was necessary to use the
    library from a CDN.

--- extension.php
+++ extension.php
@@ -1,6 +1,12 @@
 <?php
 
 class LatexSupportExtension extends Minz_Extension {
+    private const MATHJAX_URL = 'https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js';
+
+    protected array $csp_policies = [
+        'default-src' => 'https://cdn.jsdelivr.net',
+    ];
+
     public function init() {
         $this->loadMathjax();
         $this->registerHook('entry_before_display', array($this, 'sanitize'));
@@ -9,22 +15,21 @@ class LatexSupportExtension extends Minz_Extension {
     private function loadMathjax() {
         $config = 'mathjax-config.js';
         Minz_View::appendScript($this->getFileUrl($config, 'js'));
-        $lib = 'mathjax/tex-chtml.js';
-        Minz_View::appendScript($this->getFileUrl($lib, 'js'));
+        Minz_View::appendScript(self::MATHJAX_URL);
     }
 
     public function sanitize($entry) {
-        $content = str_replace(array(
+        $content = str_replace([
             '\\left⌊',
             '\\right⌋',
             '\\Complex',
             '\\Reals'
-        ), array(
+        ], [
             '\\left\\lfloor',
             '\\right\\rfloor',
             '\mathbb{C}',
             '\mathbb{R}',
-        ), $entry->content());
+        ], $entry->content());
 
         $entry->_content($content);
 
--- static/mathjax-config.js
+++ static/mathjax-config.js
@@ -1,4 +1,3 @@
-// https://github.com/mathjax/MathJax/releases/tag/3.0.5
 window.MathJax = {
   tex: {
     inlineMath: [['$', '$'], ['\\(', '\\)']]
